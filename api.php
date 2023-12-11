5b86e5de9189c48f3ba48720b918f426f05d809b
schmuhl <schmuhl@gmail.com>
2023-12-11 12:37:26 -0700

    Ignore folders in the art folder

--- api.php
+++ api.php
@@ -2,7 +2,13 @@
 
 $directory = 'art';
 $files = scandir($directory); // Get file listing
-$files = array_diff($files, array('..','.','.DS_Store') );
+$temp = array();
+foreach ( $files as $file ) {
+  if ( $file == '.DS_Store' || is_dir($directory.'/'.$file) ) continue;
+  $temp []= $file;
+}
+$files = $temp;
+
 header("HTTP/1.1 200 OK");
 header('Content-type: text/json');
 echo json_encode(array_values($files)); // Encode the file listing as JSON
