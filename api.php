ce91c88d4e86398ecde94c2e5e979b1dc44230d1
schmuhl <schmuhl@gmail.com>
2024-10-15 15:25:43 -0600

    Updates, including month folders

--- api.php
+++ api.php
@@ -1,15 +1,30 @@
 <?php
 
-$directory = 'art';
-$files = scandir($directory); // Get file listing
-$temp = array();
-foreach ( $files as $file ) {
-  if ( $file == '.DS_Store' || is_dir($directory.'/'.$file) || substr($file,0,2) == '._' ) continue;
-  $temp []= $file;
-}
-$files = $temp;
+// get the art
+$files = getArt('art');  // from the main folder, works for every day of the year
+$today = getdate();
+$files = array_merge($files,getArt('art/'.$today['mon']));  // monthly art
+$files = array_merge($files,getArt('art/'.$today['mon'].'/'.$today['mday']));  // daily art
+//print_r($files);
 
+// show the data
 header("HTTP/1.1 200 OK");
 header('Content-type: text/json');
 echo json_encode(array_values($files)); // Encode the file listing as JSON
 exit();
+
+
+
+
+function getArt ( $directory ) {
+  //echo "\nLooking in $directory ...";
+  if ( !is_dir($directory) ) return array();
+  $files = scandir($directory); // Get file listing
+  $temp = array();
+  foreach ( $files as $file ) {
+    if ( $file == '.DS_Store' || is_dir($directory.'/'.$file) || substr($file,0,2) == '._' ) continue;
+    $temp []= $file;
+  }
+  //echo " found ".count($temp)." files.";
+  return $temp;
+}
